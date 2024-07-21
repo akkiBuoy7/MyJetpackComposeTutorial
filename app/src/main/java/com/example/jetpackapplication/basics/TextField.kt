6f1c97cb9107df5238a490eb81538eaf92a8f80a
akkiB <akash3350saha@gmail.com>
2024-07-21 12:43:37 +0530

    Password Textfield

--- app/src/main/java/com/example/jetpackapplication/basics/TextField.kt
+++ app/src/main/java/com/example/jetpackapplication/basics/TextField.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Icon
@@ -20,16 +21,41 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetpackapplication.R
 
 @Composable
 fun MyTextField() {
+	Column(
+		modifier = Modifier.fillMaxSize() ,
+		verticalArrangement = Arrangement.Center ,
+		horizontalAlignment = Alignment.CenterHorizontally
+	) {
+
+		MyTextFieldBasic()
+
+		MyOutlineTextField()
+
+		MyPasswordTextField()
+
+	}
+
+}
+
+@Composable
+private fun MyTextFieldBasic(
+) {
 	var text by remember {
 		mutableStateOf("")
 	}
@@ -39,7 +65,51 @@ fun MyTextField() {
 	var enableState by remember {
 		mutableStateOf(false)
 	}
+	TextField(value = text , onValueChange = { newText ->
+		text = newText
+	} , placeholder = {
+		Text(text = "Type here...")
+	} , label = {
+		Text(text = "Email")
+	} , leadingIcon = {
+		Icon(
+			imageVector = Icons.Filled.Email , contentDescription = "emailIcon"
+		)
+	} , trailingIcon = {
+		IconButton(onClick = {
+			readOnlyState = !readOnlyState
+			enableState = !enableState
+		}) {
+			Icon(imageVector = Icons.Filled.Edit , contentDescription = "editIcon")
+		}
+	} ,
+		//readOnly = readOnlyState,
+		      enabled = enableState , // will not even focus until switched on
+		      modifier = Modifier.background(Color.Magenta) ,
+		      colors = TextFieldDefaults.textFieldColors(
+			      textColor = Color.White ,
+			      leadingIconColor = Color.White ,
+			      trailingIconColor = Color.Black ,
+			      disabledTextColor = Color.Yellow ,
+			      unfocusedLabelColor = Color.Yellow ,
+			      focusedLabelColor = Color.White
+		      ) ,
+		      keyboardOptions = KeyboardOptions(
+			      keyboardType = KeyboardType.Email ,
+			      imeAction = ImeAction.Done
+		      ) ,
+		      keyboardActions = KeyboardActions(
+			      onDone = {
+				      Log.d("Done IME" , "Done Clicked")
+				      readOnlyState = !readOnlyState
+				      enableState = !enableState
+			      }
+		      ))
+}
 
+@Composable
+private fun MyOutlineTextField(
+) {
 	var textO by remember {
 		mutableStateOf("")
 	}
@@ -49,100 +119,93 @@ fun MyTextField() {
 	var enableStateO by remember {
 		mutableStateOf(false)
 	}
-	Column(
-		modifier = Modifier.fillMaxSize() ,
-		verticalArrangement = Arrangement.Center ,
-		horizontalAlignment = Alignment.CenterHorizontally
-	) {
+	OutlinedTextField(value = textO , onValueChange = { newText ->
+		textO = newText
+	} , placeholder = {
+		Text(text = "Type here...")
+	} , label = {
+		Text(text = "Email")
+	} , leadingIcon = {
+		Icon(
+			imageVector = Icons.Filled.Email , contentDescription = "emailIcon"
+		)
+	} , trailingIcon = {
+		IconButton(onClick = {
+			readOnlyStateO = !readOnlyStateO
+			enableStateO = !enableStateO
+		}) {
+			Icon(imageVector = Icons.Filled.Edit , contentDescription = "editIcon")
+		}
+	} ,
+	                  readOnly = readOnlyStateO , // will focus only can not change value
+		//enabled = enableStateO,
+		              modifier = Modifier.background(Color.LightGray) ,
+		              colors = TextFieldDefaults.textFieldColors(
+			              textColor = Color.White ,
+			              leadingIconColor = Color.White ,
+			              trailingIconColor = Color.Black ,
+			              disabledTextColor = Color.Yellow ,
+			              unfocusedLabelColor = Color.Yellow ,
+			              focusedLabelColor = Color.White
+		              ) ,
+		              keyboardOptions = KeyboardOptions(
+			              keyboardType = KeyboardType.Email ,
+			              imeAction = ImeAction.Done
+		              ) ,
+		              keyboardActions = KeyboardActions(
+			              onDone = {
+				              Log.d("Done IME" , "Done Clicked")
+				              readOnlyStateO = !readOnlyStateO
+				              enableStateO = !enableStateO
+			              }
+		              ))
+}
 
-		TextField(value = text , onValueChange = { newText ->
-			text = newText
-		} , placeholder = {
-			Text(text = "Type here...")
-		} , label = {
-			Text(text = "Email")
-		} , leadingIcon = {
-			Icon(
-				imageVector = Icons.Filled.Email , contentDescription = "emailIcon"
-			)
-		} , trailingIcon = {
-			IconButton(onClick = {
-				readOnlyState = !readOnlyState
-				enableState = !enableState
-			}) {
-				Icon(imageVector = Icons.Filled.Edit , contentDescription = "editIcon")
-			}
-		} ,
-			//readOnly = readOnlyState,
-			      enabled = enableState ,
-			      modifier = Modifier.background(Color.Magenta) ,
-			      colors = TextFieldDefaults.textFieldColors(
-				      textColor = Color.White ,
-				      leadingIconColor = Color.White ,
-				      trailingIconColor = Color.Black ,
-				      disabledTextColor = Color.Yellow ,
-				      unfocusedLabelColor = Color.Yellow ,
-				      focusedLabelColor = Color.White
-			      ) ,
-			      keyboardOptions = KeyboardOptions(
-				      keyboardType = KeyboardType.Email ,
-				      imeAction = ImeAction.Done
-			      ) ,
-			      keyboardActions = KeyboardActions(
-				      onDone = {
-					      Log.d("Done IME" , "Done Clicked")
-					      readOnlyState = !readOnlyState
-					      enableState = !enableState
-				      }
-			      ))
-
-		OutlinedTextField(value = textO , onValueChange = { newText ->
-			textO = newText
-		} , placeholder = {
-			Text(text = "Type here...")
-		} , label = {
-			Text(text = "Email")
-		} , leadingIcon = {
-			Icon(
-				imageVector = Icons.Filled.Email , contentDescription = "emailIcon"
-			)
-		} , trailingIcon = {
-			IconButton(onClick = {
-				readOnlyStateO = !readOnlyStateO
-				enableStateO = !enableStateO
-			}) {
-				Icon(imageVector = Icons.Filled.Edit , contentDescription = "editIcon")
-			}
-		} ,
-		                  readOnly = readOnlyStateO ,
-			//enabled = enableStateO,
-			              modifier = Modifier.background(Color.LightGray) ,
-			              colors = TextFieldDefaults.textFieldColors(
-				              textColor = Color.White ,
-				              leadingIconColor = Color.White ,
-				              trailingIconColor = Color.Black ,
-				              disabledTextColor = Color.Yellow ,
-				              unfocusedLabelColor = Color.Yellow ,
-				              focusedLabelColor = Color.White
-			              ) ,
-			              keyboardOptions = KeyboardOptions(
-				              keyboardType = KeyboardType.Email ,
-				              imeAction = ImeAction.Done
-			              ) ,
-			              keyboardActions = KeyboardActions(
-				              onDone = {
-					              Log.d("Done IME" , "Done Clicked")
-					              readOnlyStateO = !readOnlyStateO
-					              enableStateO = !enableStateO
-				              }
-			              ))
-
+@Composable
+fun MyPasswordTextField() {
+    // remembers in case of configuration changes also
+	var password by rememberSaveable {
+		mutableStateOf("")
 	}
+
+	var passwordVisibility by remember {
+		mutableStateOf(false)
+	}
+
+	var icon = if (passwordVisibility) {
+		painterResource(id = R.drawable.ic_visible)
+
+	} else {
+		painterResource(id = R.drawable.ic_visible_off)
+	}
+
+	OutlinedTextField(
+		value = password ,
+		onValueChange = { newPassword ->
+			password = newPassword
+		} ,
+		placeholder = { Text(text = "Password") } ,
+		label = { Text(text = "Password") } ,
+		trailingIcon = {
+			IconButton(
+				onClick = { passwordVisibility = !passwordVisibility } ,
+				modifier = Modifier.size(30.dp)
+			) {
+				Icon(
+					painter = icon ,
+					contentDescription = "VisibleIcon"
+				)
+			}
+		} ,
+		keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password) ,
+		visualTransformation = if (passwordVisibility) VisualTransformation.None
+		else PasswordVisualTransformation()
+	)
 
 }
 
 @Preview
 @Composable
 private fun PreviewTextField() {
-	MyTextField()
+	MyPasswordTextField()
 }
